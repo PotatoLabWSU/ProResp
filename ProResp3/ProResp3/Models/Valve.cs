9d52855ae68df281aa6afafcc86971136034379c
zach6215 <zach6215@gmail.com>
2023-12-05 05:01:13 -0800

    Remove unused libraries.

--- ProResp3/ProResp3/Models/Valve.cs
+++ ProResp3/ProResp3/Models/Valve.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ProResp3.Models
 {
-    using System.ComponentModel;
     public class Valve
     {
         private double cO2;
